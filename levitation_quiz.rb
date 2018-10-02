3cac60e9b1fdcd00b012ec617baab89446ee9fd5
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-02 17:40:45 +0000

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,12 @@
 
 def levitation_quiz
-	#your code here
+	do 
+	  puts "What is the spell that enacts levitation?"
+	  ans = gets.chomp
+	  if ans == "Wingardium Leviosa"
+	    break
+	  end
+	end
 end
 
 
